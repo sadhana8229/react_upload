7b78da5eb83647d131c353bd2167a2d33b30692b
surajtriazine <surajs@triazinesoft.com>
2022-07-07 11:05:06 +0530

    try

---
+++ react/router/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Menu from './Component/Router/Menu';
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './Component/Screens/Homepage';
+import About from './Component/Screens/About';
+import Services from './Component/Screens/Services';
+import Contact from './Component/Screens/Contact';
+import Signin from './Component/Signin';
+
+
+function App() {
+  return (
+      <>
+      <div>
+      <Menu />
+      
+      <Routes>
+          <Route path="/" element={<Homepage />} exact />
+          <Route path="/about" element={<About />} exact />
+          <Route path="/services" element={<Services />} exact />
+          <Route path="/contact" element={<Contact />} exact />
+          <Route path="/signin" element={<Signin />}></Route>
+      </Routes>
+      </div>
+      
+      
+      </>
+  );
+}
+
+export default App;
